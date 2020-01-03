ebb4168d1f113a0272050a99aaf840d205e45aef
oleksastepaniuk <stepaniuq@gmail.com>
2020-01-03 22:16:36 +0200

    updated pre-processing for sankey (detailed and simplified html)

--- BI_Sankey/sankey_preparing_data.R
+++ BI_Sankey/sankey_preparing_data.R
@@ -3,8 +3,10 @@
 
 library(stringr)
 library(networkD3)
+library(htmlwidgets) # to save network with saveWidget
 library(dplyr)
 library(tidyr)
+
 
 ############## A database with the MPs info ##############
 
@@ -24,7 +26,7 @@ df_mp <- df_mp[df_mp$faction!='Institution',]
 
 # saveRDS(df_mp, file = "df_database.rds")
 
-write.csv(df_mp, file='df_database.csv', row.names = FALSE)
+write.csv(df_mp, file='./BI_Sankey_code/www/df_database.csv', row.names = FALSE)
 
 ############## DF with only faction info ##############
 
@@ -49,6 +51,7 @@ df_zero$target_new <- 'new_MPs'
 df_total <- rbind(df_total, df_zero)
 
 df_total$name_full <- paste(df_total$target_new, df_total$rada_number, sep='_')
+df_total$name_full <- ifelse(df_total$target_new=='new_MPs', 'new_MPs', df_total$name_full)
 df_total$rada_number <- as.numeric(df_total$rada_number)
 
 rm(df_zero)
@@ -64,17 +67,16 @@ nodes <- data.frame(
 )
 
 rada_numbers <- unique(df_total$rada_number)
+rada_numbers <- rada_numbers[!is.na(rada_numbers)]
 
 
 ###### Initial data
-Rada <- rada_numbers[1]
-target_name <- paste('target', Rada,sep='_')
-prepare_links <- df_total[df_total$rada_number==Rada,c('name_eng','name_full')]
-colnames(prepare_links) <- c('name_eng',target_name)
+prepare_links <- df_total[df_total$name_full=='new_MPs',c('name_eng','name_full')]
+colnames(prepare_links) <- c('name_eng', 'target_0')
 
 
 ####### Every real Rada
-for(Rada in rada_numbers[2:length(rada_numbers)]){
+for(Rada in rada_numbers){
   next_Rada <- df_total[df_total$rada_number==Rada,c('name_eng','name_full')]
   prepare_links <- merge(prepare_links, next_Rada,
                          by=c('name_eng'), all.x=TRUE)
@@ -126,8 +128,8 @@ links$value <- 0.5
 ############### Saving data #####################
 
 ######
-saveRDS(links, file = "links_total.rds")
-saveRDS(nodes, file = "nodes_total.rds")
+saveRDS(links, file = "./BI_Sankey_code/www/links_total.rds")
+saveRDS(nodes, file = "./BI_Sankey_code/www/nodes_total.rds")
 
 ######
 convocation_factions <- subset(df_total,
@@ -136,9 +138,89 @@ convocation_factions <- subset(df_total,
 
 convocation_factions <- convocation_factions[convocation_factions$rada_number!=0,]
 
-saveRDS(convocation_factions, file = "convocation_factions.rds")
+saveRDS(convocation_factions, file = "./BI_Sankey_code/www/convocation_factions.rds")
 
 ######
-saveRDS(prepare_links, file = "prepare_links.rds")
+saveRDS(prepare_links, file = "./BI_Sankey_code/www/prepare_links.rds")
+
+
+##################### Detailed diagram
+
+# Make the Network
+diagram <- sankeyNetwork(Links = links, Nodes = nodes,
+                         Source = "IDsource", Target = "IDtarget",
+                         Value = "value", NodeID = "name", 
+                         sinksRight=FALSE,
+                         fontSize = 14,
+                         nodeWidth = 15, width=1500, height=750)
+
+# save
+file_name <- ".\\BI_Sankey_code\\www\\big_picture_detailed.html"
+saveWidget(diagram, file.path(normalizePath(dirname(file_name)),basename(file_name)), selfcontained = TRUE)
+rm(file_name)
+unlink(".\\BI_Sankey_code\\www\\big_picture_detailed_files", recursive=TRUE) # in addition ti html file a folder is created
+
+
+
+############################### Simplified network
+
+prepare_simplified <- prepare_links
+nodes <- c('new_MPs')
+
+for(target in target_columns){
+  # Get the convocation number
+  convocation <- as.numeric(str_extract(target, "\\d+"))
+  convocation <- paste('Rada', convocation, sep='_')
+  nodes <- c(nodes, convocation)
+  prepare_simplified[,target] <- ifelse(is.na(prepare_simplified[,target]), NA, convocation)
+  print(convocation)
+}
+
+nodes <- data.frame(
+  name=nodes
+)
+
+first_Rada <- TRUE
+
+for(i in seq(length(target_columns))){
+  target <- target_columns[i]
+  sources <- source_columns[1:i]
+  
+  df_target <- prepare_simplified[!is.na(prepare_simplified[,target]),c(sources,target)]
+  
+  if(first_Rada){
+    links <- df_target
+    colnames(links) <- c('source', 'target')
+    first_Rada <- FALSE
+  } else {
+    df_target$source <- apply(df_target[,sources], 1, find_source)
+    df_target <- df_target[,c(target,'source')]
+    colnames(df_target) <- c('target','source')
+    links <- rbind(links,df_target)
+  }
+}
+rm(i,sources,target)
+
+links$IDsource <- match(links$source, nodes$name)-1 
+links$IDtarget <- match(links$target, nodes$name)-1
+
+links$value <- 0.5
+
+# Make the Network
+diagram <- sankeyNetwork(Links = links, Nodes = nodes,
+                         Source = "IDsource", Target = "IDtarget",
+                         Value = "value", NodeID = "name", 
+                         sinksRight=FALSE,
+                         fontSize = 14,
+                         nodeWidth = 20, width=1000, height=500)
+
+# save
+file_name <- ".\\BI_Sankey_code\\www\\big_picture_simplified.html"
+saveWidget(diagram, file.path(normalizePath(dirname(file_name)),basename(file_name)), selfcontained = TRUE)
+rm(file_name)
+unlink(".\\BI_Sankey_code\\www\\big_picture_simplified_files", recursive=TRUE) # in addition ti html file a folder is created
+
+
+
 
 
